f8336b2efea6fa352c3c5d1b8a3de73fc6b692eb
milena-mognon <milenamognon@gmail.com>
2022-01-09 16:14:47 -0300

    Aula 4 - Desafios resolvidos

--- banco-de-dados/src/routes/index.routes.ts
+++ banco-de-dados/src/routes/index.routes.ts
@@ -34,4 +34,65 @@ routes.get("/users", async (request: Request, response: Response) => {
   return response.json(users); // retorna os usuário no formato JSON
 });
 
+/**
+ * Atualiza um usuário
+ */
+routes.put("/users/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const data = request.body;
+
+  let ormRepository: Repository<User>; // váriavel do tipo repositório de User
+
+  ormRepository = getRepository(User); // conecta com a tabela users do banco de dados
+
+  const dataToUpdate = {
+    id: Number(id), // precisa converter para Number
+    ...data,
+  };
+
+  const user = await ormRepository.save(dataToUpdate); // atualiza o usuário
+
+  return response.json(user); // retorna os usuário no formato JSON
+});
+
+/**
+ * Busca um Usuário
+ */
+routes.get("/users/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  let ormRepository: Repository<User>; // váriavel do tipo repositório de User
+
+  ormRepository = getRepository(User); // conecta com a tabela users do banco de dados
+
+  // Primira opção para buscar um usuário
+  const user = await ormRepository.findOne(id); // busca um usuário
+
+  // Segunda opção para buscar um usuário
+  // const user = await ormRepository.findOne({ where: { id } }); // busca um usuário
+
+  // Terceira opção para buscar um usuário
+  // const user = await ormRepository
+  //   .createQueryBuilder("users")
+  //   .where("users.id = :id", { id })
+  //   .getOne(); // busca um usuário
+
+  return response.json(user); // retorna os usuário no formato JSON
+});
+
+/**
+ * Busca um Usuário
+ */
+routes.delete("/users/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  let ormRepository: Repository<User>; // váriavel do tipo repositório de User
+
+  ormRepository = getRepository(User); // conecta com a tabela users do banco de dados
+
+  const user = await ormRepository.delete(id); // deleta um usuário
+
+  return response.json(user); // retorna os usuário no formato JSON
+});
+
 export default routes;
